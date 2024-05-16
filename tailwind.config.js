a570fcfb197d1ee4430f894a975b25e311657948
Vilma Ponce <115092661+vilmaponce@users.noreply.github.com>
2024-05-16 11:01:53 -0300

    Proyecto Gym

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html','./clases.html','./horarios.html', './membresia.html' , './tienda.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
